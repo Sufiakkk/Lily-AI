8af800d77ed6e94650719c8f4a48c7bb41671155
Sufiakkk <151846650+Sufiakkk@users.noreply.github.com>
2025-04-20 11:10:46 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,18 @@
-const CACHE_NAME = "lily-cache-v1";
-const ASSETS = [
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  // add any CSS/JS/assets you host
-];
-
-self.addEventListener("install", evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("lily-cache").then(cache => {
+      return cache.addAll([
+        "dashboard.html",
+        "style.css",
+        "main.js",
+        "manifest.json"
+      ]);
+    })
   );
 });
 
-self.addEventListener("activate", evt => {
-  evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-self.addEventListener("fetch", evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cached =>
-      cached || fetch(evt.request)
-    )
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
