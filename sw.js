4fa01023e98780a346ffe0a40d0a692bb45a7e6d
Sufiakkk <151846650+Sufiakkk@users.noreply.github.com>
2025-04-20 18:20:23 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,1 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('chat-cache').then((cache) => {
-            return cache.addAll([
-                '/index.html',
-                '/style.css',
-                '/main.js'
-            ]);
-        })
-    );
-});
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
-});
